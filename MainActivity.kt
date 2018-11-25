8b5e00d5a8e865458904688c5dfb751e1db67d64
sh <ganeshsalave123@gmail.com>
2018-11-24 17:05:48 -0800

    ok first commit

--- MainActivity.kt
+++ MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 super.onPageStarted(view, url, favicon)
                 pDialog?.show()
+
             }
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
